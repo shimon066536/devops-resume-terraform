87c901a4ae0ca8b19dbfc9716ef8bfc46d83faa2
shimon <shimon066536@gmail.com>
2025-03-18 16:37:12 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,3 +6,13 @@ terraform {
     }
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "devops-resume-${var.name}-${random_id.bucket_suffix.hex}"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
