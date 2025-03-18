5e4634eb3b16d5fc0219cd689f1a6bbaecca27e9
shimon <shimon066536@gmail.com>
2025-03-18 16:37:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,25 @@ resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "devops-resume-${var.name}-${random_id.bucket_suffix.hex}"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.mybucket.id
 
